311a921f5d25d90fa2407fd2131315b2cecf54aa
Estorba <03.markjohn.30@gmail.com>
2025-08-03 06:04:43 +0800

    updated

--- resources/views/profile/partials/vehicles.blade.php
+++ resources/views/profile/partials/vehicles.blade.php
@@ -3,13 +3,13 @@
         $vehname = app('App\Http\Controllers\ProfileController')->getVehicleName($vehicle->modelid);
     @endphp
     <div class="rounded-xl border border-gray-100/30 p-4 shadow mb-4 bg-black/20">
-        <div class="mb-4">
+        <div class="mb-4 text-center"> <!-- Center the whole text block -->
             <h3 class="text-md font-semibold text-white mb-1">
                 {{ $vehname }}
             </h3>
-            <p class="text-sm text-gray-300">Model ID: {{ $vehicle->modelid }}</p>
-            <p class="text-sm text-gray-300">Plate: {{ $vehicle->plate }}</p>
-            <p class="text-sm text-gray-300">Health: {{ $vehicle->health }}</p>
+            <p class="text-sm text-gray-300">Model ID: <span class="text-gray-500">{{ $vehicle->modelid }}</span></p>
+            <p class="text-sm text-gray-300">Plate: <span class="text-gray-500">{{ $vehicle->plate }}</span></p>
+            <p class="text-sm text-gray-300">Health: <span class="text-gray-500">{{ $vehicle->health }}</span></p>
         </div>
         <div class="flex justify-center">
             <img src="{{ asset('images/vehicles2/' . $vehicle->modelid . '.png') }}"
@@ -18,5 +18,7 @@
         </div>
     </div>
 @empty
-    <p class="text-white">No vehicles owned.</p>
+    <div class="rounded-xl border border-gray-100/30 p-8 shadow mb-4 bg-black/20 flex justify-center items-center h-40">
+        <p class="text-white text-center">No vehicles owned.</p>
+    </div>
 @endforelse
