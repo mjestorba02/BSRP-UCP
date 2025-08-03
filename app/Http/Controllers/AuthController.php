0acf718583957252eb05f3a0e9e4e268a1c3f866
Estorba <03.markjohn.30@gmail.com>
2025-08-04 02:57:17 +0800

    Test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 use App\Models\User;
 
 class AuthController extends Controller
